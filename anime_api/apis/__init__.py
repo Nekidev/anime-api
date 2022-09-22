d21393c3b6b97832ce6878d64f07fb820b13daa6
Neki <84998222+Nekidev@users.noreply.github.com>
2022-09-22 00:01:30 -0300

    Created init file in anime_api/apis

---
+++ anime_api/apis/__init__.py
@@ -0,0 +1,6 @@
+"""
+All API wrapper classes can be imported from here.
+"""
+from .trace_moe import TraceMoeAPI
+from .anime_facts_rest_api import AnimeFactsRestAPI
+from .animechan import AnimechanAPI
